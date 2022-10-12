5a72ced9e1f5c4127c135a3b5e1cfbc42c852549
Mitchell Schofield <scho.mitch@gmail.com>
2022-10-12 14:36:05 -0700

    Create helper file for availibleSpots function

--- src/components/DateListItem.jsx
+++ src/components/DateListItem.jsx
@@ -1,26 +1,13 @@
 import "./DateListItem.scss";
+import getAvailibleSpots from "../helpers/getAvailibleSpots";
 //----------------------------------------------------------------------------------------------------
 
 const DateListItem = ({ date, setAddWalkDate, walks }) => {
 
 //--------------------------------------------------------------------------------------------------
 //Update availible spots
-  
-  const walksForDate = walks.filter((walk) => {
-    return date.isSame(walk.date, "day")
-  });
-
-  const dogsForDate = walksForDate.filter((walk) => {
-    return walk.dogs;
-  })
-
-  let allDogsForDate = 0;
-  for (const item of dogsForDate) {
-      allDogsForDate += item.dogs.length; 
-  }
-
-  const availibleSpotsForDate = 12 - allDogsForDate;
-  
+ 
+  const availibleSpotsForDate = getAvailibleSpots(date, walks);
 //-----------------------------------------------------------------------------------------------------------
   const clickToAddWalk = () => {
     setAddWalkDate(date);
---
+++ src/helpers/getAvailibleSpots.js
@@ -0,0 +1,19 @@
+const getAvailibleSpots = (date, walks) => {
+  const walksForDate = walks.filter((walk) => {
+    return date.isSame(walk.date, "day")
+  });
+
+  const dogsForDate = walksForDate.filter((walk) => {
+    return walk.dogs;
+  })
+
+  let allDogsForDate = 0;
+  for (const item of dogsForDate) {
+    allDogsForDate += item.dogs.length;
+  }
+
+  const availibleSpotsForDate = 12 - allDogsForDate;
+  return availibleSpotsForDate;
+};
+
+export default getAvailibleSpots;
